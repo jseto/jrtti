83da091a5bfed636ecb60b433a39214a9e1bd0cd
jseto <jseto@jasvisio.com>
2012-11-27 15:55:35 +0100

    jrtti_cast allows casting between fundamental types

--- include/jrtti/helpers.hpp
+++ include/jrtti/helpers.hpp
@@ -4,6 +4,7 @@
 
 #include <sstream>
 #include <boost/any.hpp>
+#include <boost/type_traits/is_fundamental.hpp>
 
 namespace jrtti {
 	/**
@@ -57,47 +58,93 @@ namespace jrtti {
 		template< typename T >
 		static
 		typename boost::enable_if< typename boost::is_pointer< T >::type, T >::type
-			__jrtti_cast( const boost::any& value ) {
-				if ( value.empty() ) {
-					return NULL;
-				}
+		__jrtti_cast( const boost::any& value ) {
+			if ( value.empty() ) {
+				return NULL;
+			}
 
-				if ( value.type() == typeid( void * ) ) {
-					return ( T )boost::any_cast< void * >( value );
-				}
+			if ( value.type() == typeid( void * ) ) {
+				return ( T )boost::any_cast< void * >( value );
+			}
 
-				if ( value.type() == typeid( T ) ) {
-					return boost::any_cast< T >( value );
-				}
+			if ( value.type() == typeid( T ) ) {
+				return boost::any_cast< T >( value );
+			}
 
-				if ( ( typeid( T ) == typeid( void * ) ) ) {
-					return (T)*boost::unsafe_any_cast< void * >( &value );
-				}
+			if ( ( typeid( T ) == typeid( void * ) ) ) {
+				return (T)*boost::unsafe_any_cast< void * >( &value );
+			}
 
-				Metatype& value_mt = Reflector::instance().metatype( value.type() );
-				Metatype& templT_mt = Reflector::instance().metatype< T >();
-				if ( value_mt.isDerivedFrom( templT_mt ) || templT_mt.isDerivedFrom( value_mt ) ) {
-					return (T)*boost::unsafe_any_cast< void * >( &value );
-				}
+			Metatype& value_mt = Reflector::instance().metatype( value.type() );
+			Metatype& templT_mt = Reflector::instance().metatype< T >();
+			if ( value_mt.isDerivedFrom( templT_mt ) || templT_mt.isDerivedFrom( value_mt ) ) {
+				return (T)*boost::unsafe_any_cast< void * >( &value );
+			}
 
-				throw BadCast( typeid( T ).name() );
+			throw BadCast( typeid( T ).name() );
 		}
 
 		//jrtti_cast SFINAE for non-pointers 
 		template< typename T >
 		static
 		typename boost::disable_if< typename boost::is_pointer< T >::type, T >::type
-			__jrtti_cast( const boost::any& value ) {
-				if ( value.type() == typeid( T ) ) {
-					return ( T )boost::any_cast< T >( value );
-				}
-				if ( value.type() == typeid( T * ) ) {
-					return * ( T * )boost::any_cast< T * >( value );
-				}
-				if ( value.type() == typeid( void * ) ) {
-					return * ( T * )boost::any_cast< void * >( value );
-				}
-				throw BadCast( typeid( T ).name() );
+		__jrtti_cast( const boost::any& value ) {
+			if ( value.type() == typeid( T ) ) {
+				return ( T )boost::any_cast< T >( value );
+			}
+			if ( value.type() == typeid( T * ) ) {
+				return * ( T * )boost::any_cast< T * >( value );
+			}
+			if ( value.type() == typeid( void * ) ) {
+				return * ( T * )boost::any_cast< void * >( value );
+			}
+			if ( boost::is_fundamental< T >::value ) {
+				return tryFundamental< T >( value );
+			}
+			throw BadCast( typeid( T ).name() );
+		}
+
+		// SFINAE for tryFundamental for fundamental types
+		template< typename T >
+		static
+		typename boost::enable_if< typename boost::is_fundamental< T >::type, T>::type
+		tryFundamental( const boost::any& value ) {
+#pragma warning( push )
+#pragma warning( disable:4800 )
+			if ( value.type() == typeid( char ) ) {
+				return static_cast< T >( boost::any_cast< char >( value ) );
+			}
+			if ( value.type() == typeid( bool ) ) {
+				return static_cast< T >( boost::any_cast< bool >( value ) );
+			}
+			if ( value.type() == typeid( short ) ) {
+				return static_cast< T >( boost::any_cast< short >( value ) );
+			}
+			if ( value.type() == typeid( int ) ) {
+				return static_cast< T >( boost::any_cast< int >( value ) );
+			}
+			if ( value.type() == typeid( long ) ) {
+				return static_cast< T >( boost::any_cast< long >( value ) );
+			}
+			if ( value.type() == typeid( float ) ) {
+				return static_cast< T >( boost::any_cast< float >( value ) );
+			}
+			if ( value.type() == typeid( double ) ) {
+				return static_cast< T >( boost::any_cast< double >( value ) );
+			}
+			if ( value.type() == typeid( long double ) ) {
+				return static_cast< T >( boost::any_cast< long double >( value ) );
+			}
+#pragma warning( pop )
+			throw BadCast( typeid( T ).name() );
+		}
+
+		// SFINAE for tryFundamental for fundamental types
+		template< typename T >
+		static
+		typename boost::disable_if< typename boost::is_fundamental< T >::type, T>::type
+		tryFundamental( const boost::any& value ) {
+			throw BadCast( typeid( T ).name() );
 		}
 	};
 }; // namespace jrtti
